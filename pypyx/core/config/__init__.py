207b4ef8e816850f130279a85ab982dead90b6f9
Zytro <omarwt92@gmail.com>
2022-04-17 11:51:30 +0800

    Update pypyx\core\config\__init__.py

--- pypyx/core/config/__init__.py
+++ pypyx/core/config/__init__.py
@@ -1,2 +1,19 @@
 # Importing
+import os
 import sys
+
+from configparser import ConfigParser
+
+
+# Config class
+class Config(sys.modules[__name__].__class__):
+    # Getting the configuration file
+    file = ConfigParser()
+
+    path = f"{os.getcwd()}\\pypyx.ini"
+    default_path = f"{os.path.dirname(__file__)}\\pypyx.ini"
+
+    file.read([default_path, path])
+
+
+sys.modules[__name__].__class__ = Config
