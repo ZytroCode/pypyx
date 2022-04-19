9662bc6c08a8bf924878daebb02133a39f6ee546
Zytro <omarwt92@gmail.com>
2022-04-19 14:48:27 +0800

    Added author and docstring

--- pypyx/__init__.py
+++ pypyx/__init__.py
@@ -1,10 +1,17 @@
+"""PyPyx is a python library used for making games with the help of pygame.
+
+PyPyx is a 3rd party open-source python library that unlocks the potential of your games.
+With PyPyx's features, the limits of your games are unlimited.
+"""
+
 # Configs
 __version__ = "0.2.0"
+__author__ = "ZytroCode"
 __all__ = []
 
 # Core
 from pypyx import core
 
-# Importing
+# Tools
 from pygame import _sdl2 as SDL2
 from pypyx import utils
