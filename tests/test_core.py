b0115936f35270359d4e5f56ab73245e42efeb4d
Zytro <omarwt92@gmail.com>
2022-04-19 11:20:50 +0800

    Updated tests for more coverage

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,5 @@
 import pypyx
 
-from pypyx.utils.window import Window
 
-
-def test_core():
-    window = Window()
+def test_quit():
     pypyx.core.quit()
---
+++ tests/test_utils_window.py
@@ -0,0 +1,6 @@
+import pypyx
+
+
+def test_events():
+    window = pypyx.utils.Window()
+    window.events
