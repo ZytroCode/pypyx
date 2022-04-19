9927e8c4f69b33bca0c666c88f1f524b52329725
Zytro <omarwt92@gmail.com>
2022-04-19 10:47:38 +0800

    Added more parameters for Window and display

--- pypyx/utils/window.py
+++ pypyx/utils/window.py
@@ -5,7 +5,13 @@ from pypyx import SDL2
 from typing import Any
 
 
-class Window:
-    def __init__(self):
-        self.display = SDL2.Window()
         self.renderer = SDL2.Renderer(self.display)
+class Window(object):
+    def __init__(self, **keys) -> None:
+        # Setting up display
+        self.display = SDL2.Window(resizable=keys.get("resizable", True), hidden=True)
+        self.display.title = keys.get("title", "PyPyx Window")
+        self.width = keys.get("width", 800)
+        self.height = keys.get("height", 600)
+        self.display.size = keys.get("size", (self.width, self.height))
+        self.display.position = keys.get("position", SDL2.WINDOWPOS_CENTERED)
